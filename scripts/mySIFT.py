c33e5fd2458ce6905f5bfe15374a730c3368f2e5
abdo_mostafa <abdelrahman.mostafa.sayed@gmail.com>
2016-04-14 20:12:42 +0200

    Testing SIFT for first feature

--- scripts/mySIFT.py
+++ scripts/mySIFT.py
@@ -43,45 +43,55 @@ class mySIFT :
             featureMagnitude = self.magnitude[(row-8):(row+8) ,(col-8):(col+8) ]
             featureMagnitude = np.matrix( featureMagnitude , copy = True )
 
+            gaussianKernel1D = cv2.getGaussianKernel(16, 1.5)
+            gaussianKernel2D = gaussianKernel1D * gaussianKernel1D.transpose()
+
+            featureMagnitudeWeighted = featureMagnitude * gaussianKernel2D
 
             featurePhase = self.phaseImage[ (row-8):(row+8) , (col-8):(col+8) ]
             featurePhase =  np.matrix( featurePhase , copy = True )
 
             # get the main orientation
-            angleFrequency = np.zeros(37)
+            angleFrequency = np.zeros(36)
             dominantAngle = 0
-            gradients=np.zeros(37)
-            for index in range(37):
+            gradients=np.zeros(36)
+            for index in range(36):
                 angle = index * 10                                   #[0 10,20,30,40,........,350]
                 for i in range(featurePhase.shape[0]):
                     for j in range(featurePhase.shape[1]):
                         if featurePhase[i,j] == angle:
                             angleFrequency[index] += 1
-                            gradients[index] += featureMagnitude[i,j]
+                            gradients[index] += featureMagnitudeWeighted[i,j]
 
-            # print angleFrequency
-            # print np.max(angleFrequency)
-            # dominantAngle=np.argmax(angleFrequency)*10
-            # print dominantAngle
-
-            # print gradients
             featureMagnitudeMax = np.max(gradients)
             dominantAngle = np.argmax(gradients) * 10
-            # print dominantAngle
-            # print featureMagnitudeMax
+            print "dominat Angle" , dominantAngle
 
             # subtract the dominant angle from feature phase
-            featurePhase = featurePhase - dominantAngle
-            # print featurePhase
+            featurePhaseAdjusted = featurePhase - dominantAngle
+
             # Down sampling the 36-bin to 8-bin
-            featurePhase8Bin = ((45 * np.round(self.phaseImage / 45.0)) )%360
-            print featurePhase8Bin
-            gaussianKernel1D = cv2.getGaussianKernel( 16 , 1.5 )
-            gaussianKernel2D = gaussianKernel1D * gaussianKernel1D.transpose()
-            # print np.max(gaussianKernel2D)
-            featureMagnitude *=gaussianKernel2D
+            featurePhase8Bin = ((45 * np.round(featurePhaseAdjusted / 45.0)) ) % 360
+            print featurePhase8Bin.shape
+            print featureMagnitudeWeighted.shape
+
+            # get gradients magnitude for each 4*4 region
+            # test first quad
+            featurePhaseQuad = featurePhase8Bin[0:4 , 0:4]
+            featureMagnitudeWeightedQuad=featureMagnitudeWeighted[0:4 , 0:4]
+            print featureMagnitudeWeightedQuad
+            print featurePhaseQuad
 
 
+            featureGradients = np.zeros(8)
+            for index in range(8):
+                angle = index * 45  # [0  45 90 135 ... 315]
+                for i in range(featurePhaseQuad.shape[0]):
+                    for j in range(featurePhaseQuad.shape[1]):
+                        if featurePhaseQuad[i, j] == angle:
+                            gradients[index] += featureMagnitudeWeighted[i, j]
+
+            print featureGradients
 
     def getSIFTDescriptors(self):
         self.__featureDescription__()
