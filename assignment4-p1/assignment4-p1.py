cb1cfd2a826d48a51369f03a7478c80b21459073
abdelrahman-mostafa <abdelrahman.mostafa.sayed@gmail.com>
2016-05-02 19:15:31 +0200

    part 1 : Adding optimal thresholding method

---
+++ assignment4-p1/assignment4-p1.py
@@ -0,0 +1,56 @@
+import cv2 as cv
+import numpy as np
+import copy
+
+# optimalThresholding Takes an original image
+# to be thresholded and threshold it using
+# optimal thresholding method
+# returns optimal threshold , thresholded image
+def optimal_thresholding(image):
+    rows = image.shape[0]
+    cols = image.shape[1]
+    #  Extract initial background  (4corners)
+    background = [image[0, 0], image[0, cols-1], image[rows-1, 0], image[rows-1, cols-1]]
+    background_mean = np.mean(background)
+    # Extract initial foreground (object)
+    foreground_sum = np.sum(image) - np.sum(background)
+    foreground_mean = foreground_sum / (np.size(image) - 4)
+    thresh = (background_mean + foreground_mean) / 2
+
+    while True:
+        old_thresh = thresh
+        new_foreground = image[np.where(image > thresh)]
+        new_background = image[np.where(image < thresh)]
+        thresh = (np.mean(new_background) + np.mean(new_foreground)) / 2
+        if old_thresh == thresh:
+            break
+
+    bwImage = copy.deepcopy(image)
+    # convert the image to black and white image
+    for r in range(0, image.shape[0]):
+        for c in range(0, image.shape[1]):
+            if image[r, c] > thresh:
+                bwImage[r, c] = 255
+            else:
+                bwImage[r, c] = 0
+
+    return bwImage, thresh
+
+
+# load gray images
+imageColor = cv.imread('../images/assignment4/0019hv3.bmp')
+imageGray = cv.imread('../images/assignment4/0019hv3.bmp', cv.IMREAD_GRAYSCALE)
+
+# threshold gray image using optiaml thresholding method
+thresholded_image , optimal_threshold = optimal_thresholding(imageGray)
+
+print "optimal threshold", optimal_threshold
+
+cv.namedWindow('original image',cv.WINDOW_NORMAL)
+cv.imshow('original image', imageGray)
+
+cv.imshow('thresholded image using optimal thresholding', thresholded_image)
+
+cv.waitKey(0)
+cv.destroyAllWindows()
+
