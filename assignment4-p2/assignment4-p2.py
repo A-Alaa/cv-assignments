7d9e1911ab4aeb6e777a3ee6c687fe7babefdcf8
asem <asem.alaa.abdelaziz@gmail.com>
2016-05-03 03:01:21 +0200

    Done kmeans

--- assignment4-p2/assignment4-p2.py
+++ assignment4-p2/assignment4-p2.py
@@ -3,9 +3,7 @@ from os.path import isfile , join
 import numpy as np
 import cv2
 import sys
-
 sys.path.append('../scripts')
-
 from KmeansSegmentation import KmeansSegmentation
 
 # Get file names in "./images" directory
@@ -16,24 +14,11 @@ imageFiles = [join("../images/assignment4" , f) for f in
 image = cv2.imread(imageFiles[3])
 image = cv2.cvtColor( image , cv2.COLOR_RGB2Luv  )
 
-KmeansSegmentation( image , 2  )
+labels , centroids , wss = KmeansSegmentation( image , 3  )
 
-featureVector = image.reshape((-1 , 3))
-featureVector = np.float32(featureVector)
-
-criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER , 10 , 1.0)
-ncluster = 2
-compactness , labels , centers = cv2.kmeans(featureVector , ncluster , None ,
-                                            criteria ,
-                                            2 , cv2.KMEANS_RANDOM_CENTERS)
-centers = np.uint8( centers )
+centers = np.uint8( centroids )
 output  = centers[ labels.flatten() ]
 output  = output.reshape(( image.shape ))
-# print centers
-# print featureVector[:5]
-#
-# print image.shape
-# print featureVector.shape
 
 cv2.imshow('image' , image)
 cv2.imshow('output' , output )
