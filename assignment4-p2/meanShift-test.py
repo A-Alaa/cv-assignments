ba5bf945c6e336c1bede793eea187898f5b9da43
mohamedAbdallah <mohamed.m.i.abdallah@gmail.com>
2016-05-11 20:28:36 +0200

    Tuning Mean Shift

--- assignment4-p2/meanShift-test.py
+++ assignment4-p2/meanShift-test.py
@@ -8,63 +8,32 @@ sys.path.append('../scripts')
 
 from hisMeanShift import meanShiftSeg
 
-imagePath = "../images/assignment4/RevolutionJan25.jpg"
+imagePath = "../images/assignment4/seg1.jpg"
 
 imageRGB = cv2.imread( imagePath )
 
 imageLUV = cv2.cvtColor( imageRGB, cv2.COLOR_RGB2LUV )
 
 imageLUV2RGB = cv2.cvtColor( imageLUV, cv2.COLOR_LUV2RGB )
+# print type(imageLUV)
 
-meanShift = meanShiftSeg( imageLUV, 1 )
-
+meanShift = meanShiftSeg( imageLUV, 7 )
 segImage = meanShift.applyMeanShift()
 
-# tShit = meanShift.getSegmentedImage
-# print  type(tShit)
-print type(segImage)
-print np.shape(segImage)
-segImage = cv2.cvtColor( segImage, cv2.COLOR_LUV2RGB )
-cv2.imshow( 'image', segImage )
-# print type( imageLUV ), np.shape( imageLUV ), imageLUV.max(), imageLUV.min()
-#
-# featureVec = imageLUV.reshape( -1 ,3 )
-# # print np.shape(featureVec)
-# luvHist , edges = np.histogramdd( featureVec )
-# print np.max(imageLUV[:,:,2])
-# print type(luvHist), luvHist.shape, type(edges), np.shape(edges), luvHist
-# plt.xlim([ 0,256 ])
-# plt.plot(luvHist)
+# # print type(segImage)
+# # print np.shape(segImage)
+segImageRGB = cv2.cvtColor( segImage, cv2.COLOR_LUV2RGB )
+segimTem = cv2.cvtColor( segImageRGB, cv2.COLOR_LUV2BGR )
+cv2.imshow( 'image', segImageRGB )
+# plt.subplot(211)
+# plt.imshow( imageRGB )
+# plt.subplot(212)
+# plt.imshow( imageRGB )
 # plt.show()
-# cv2.imshow('image', luvHist)
 
-# window = np.array([[0,2,1], [4,0,5], [ 11,5,9 ]])
-# idx = range( 3 )
-# nonZeroIdx = np.transpose(np.nonzero( window ))
-# # print nonZeroIdx
-# totalMass = np.max(np.cumsum( window ))
-# print totalMass
-# momentCol = np.max(np.cumsum(window.cumsum( axis=1 )[:,3-1]*idx))
-# print momentCol
-# cntrCol = np.round(1.0*momentCol/totalMass)
-# print 1.0*momentCol/totalMass,cntrCol
+# zero = np.zeros((4,4))
+# print np.max(zero.cumsum())
 
-
-# print 1.0*256/25, 256/32
-# print a
-# print np.transpose(np.nonzero(a))
-# print np.transpose(np.nonzero(a))[:,0]
-# print a.cumsum( axis=0 )[a.shape[0]-1]
-# print a.cumsum( axis=0 )[a.shape[0]-1]*np.transpose(np.nonzero(a))[:,0]
-
-# a = np.array([[0,2,1], [4,0,5], [ 11,5,9 ]])
-# idx = range( 3 )
-# print a
-# # print np.transpose(np.nonzero(a))
-# # print np.transpose(np.nonzero(a))[:,0]
-# print a.cumsum( axis=1 )[:,2]
-# print a.cumsum( axis=1 )[:,2]*idx
-#
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
--- scripts/hisMeanShift.py
+++ scripts/hisMeanShift.py
@@ -1,4 +1,5 @@
 import numpy as np
+from matplotlib import pyplot as plt
 
 """
 An implementation to the mean-shift segmentation on LUV image
@@ -7,26 +8,48 @@ An implementation to the mean-shift segmentation on LUV image
 class meanShiftSeg:
 
     def __init__(self, image, windowSize):
-        # self.image = np.asarray( image, copy = True )
-        self.image = image
+        self.image = np.array( image, copy = True )
         assert (self.image.shape[2] == 3), "The Image must be of three channels LUV "
-        self.windowSize = 4**windowSize
-        self.segmentedImage = image
+        self.windowSize = 2**windowSize
+        print "Window size : " , self.windowSize
+        self.segmentedImage = np.array( image, copy = True )
+        ## The LUV is 256X3 , so the color space to be clustered is 256X256
         self.colorSpace = np.zeros( (256,256) )
-        self.numofClusters = np.int(256/self.windowSize)
+        self.numofClusters = np.int(256/self.windowSize)**2
+        print "# of clusters : ", self.numofClusters
         self.clustersUV = np.zeros( shape=(self.numofClusters, 2) )
-
+        # print self.numofClusters, self.windowSize,256/self.windowSize
 
 
     def __makeColorDataSpace__(self):
-        compU = np.ndarray.flatten( self.image[:,:,1] )
-        compV = np.ndarray.flatten( self.image[:,:,2] )
-        self.colorSpace[ compU,compV ] = 1
+        """
+        This function populate the color-space to be clustered
+        :return:
+        """
+
+        # compU = np.ndarray.flatten( self.image[:,:,1] )
+        # compV = np.ndarray.flatten( self.image[:,:,2] )
+        compU = np.reshape( self.image[:,:,1], (-1,1) )
+        compV = np.reshape( self.image[:,:,2], (-1,1) )
+        compUV = np.transpose(np.array((compU[:,0],compV[:,0])))
+        print compU.shape, compV.shape, compUV.shape
+        # self.colorSpace[ compU,compV ] = 1
+        for u,v in compUV :
+                # print (u, v)
+                self.colorSpace[ u,v ] += 1
+
         # print self.segmentedImage
-        # print compU.max()
-
+        # print compV.max(), compU.max()
+        # plt.plot( compV.tolist(), compU.tolist(),'bo' )
+        # plt.show()
+        # exit(1)
     def applyMeanShift(self):
-
+        """
+        Apply the mean-shift to the color-space, then classify the image U-V components
+        :return: segmented image
+        """
+        # return self.image
+        print "Apply shift .... "
         self.__makeColorDataSpace__()
         wSize = self.windowSize
         # halfWSize = self.windowSize/2
@@ -35,50 +58,74 @@ class meanShiftSeg:
         for itrRow in range( numOfWindPerDim ):
             for itrCol in range( numOfWindPerDim ):
                 cntrRow, cntrCol = self.__windowIterator__( itrRow*wSize,itrCol*wSize )
+                # print itrRow*wSize,itrCol*wSize
                 clustersTemp.append( (cntrRow, cntrCol) )
 
         self.clustersUV = np.array( clustersTemp )
-
+        print " Clusters formed "
         self.__classifyColors__()
 
         return self.segmentedImage
 
 
-    def __classifyColors__(self):
-        wSize = self.windowSize
-        numOfWindPerDim = np.int(np.sqrt( self.numofClusters ))
-        for row in range( self.image.shape[0] ):
-            for col in range( self.image.shape[1] ):
-                pixelU = self.segmentedImage[row,col,1]
-                pixelV = self.segmentedImage[row,col,2]
-                windowIdx = np.int( np.int(pixelV/wSize)%wSize  + np.int(numOfWindPerDim*( pixelU/wSize ))%wSize)
-                # print pixelV/wSize ,windowIdx, numOfWindPerDim, self.numofClusters, self.windowSize
-                # print self.clustersUV.shape
-                # exit(1)
-                self.segmentedImage[row,col,1] = self.clustersUV[windowIdx, 0]
-                self.segmentedImage[row,col,2] = self.clustersUV[windowIdx, 1]
-
 
 
     def __windowIterator__(self, row, col):
-        # print row, col
+        """
+        This function iterate in the given window indices, to find its center of mass
+        :param row:
+        :param col:
+        :return:
+        """
+        # print " Iterrating to find mean value"
         wSize = self.windowSize
         hWSize = wSize/2
         prevRow = 0
         prevCol = 0
+        # print row,":",row+wSize,col,":",col+wSize
         window = self.colorSpace[ row:row+wSize,col:col+wSize ]
-        # print window, type(window)
+        # print window.shape
         newRow, newCol = self.__findCntrMass__( window )
-        # print newRow, newCol
+        numOfIter = 0
         while( prevRow != newRow-hWSize and prevCol != newCol-hWSize ):
+            if( numOfIter > np.sqrt(self.numofClusters) ):
+                break
             # print prevRow, prevCol
             # print newRow, newCol
             prevRow = newCol-hWSize
             prevCol = newCol-hWSize
-            window = self.colorSpace[ prevRow+row:prevRow+row+wSize,prevCol+col:prevCol+col+wSize ]
+            # print numOfIter
+            # print prevRow+row,":",prevRow+row+wSize," ", prevCol+col,":", prevCol+col+wSize
+            nxtRow = (prevRow+row)%(256-wSize)
+            nxtCol = (prevCol+col)%(256-wSize)
+            window = self.colorSpace[ nxtRow:nxtRow+wSize,nxtCol:nxtCol+wSize ]
             newRow, newCol = self.__findCntrMass__( window )
-
+            numOfIter += 1
         return row + newRow, col + newCol
+
+    def __classifyColors__(self):
+            """
+            This function classify the image component based on the its value, which is the index in the color-space
+            see also : https://spin.atomicobject.com/2015/05/26/mean-shift-clustering/
+            to understand what is colo-space
+            :return:
+            """
+            wSize = self.windowSize
+            numOfWindPerDim = np.int(np.sqrt( self.numofClusters ))
+            for row in range( self.image.shape[0] ):
+                for col in range( self.image.shape[1] ):
+                    pixelU = self.segmentedImage[row,col,1]
+                    pixelV = self.segmentedImage[row,col,2]
+                    windowIdx = np.int( np.int(pixelV/wSize)  + np.int(numOfWindPerDim*( pixelU/wSize )))
+                    # print pixelV/wSize ,windowIdx, numOfWindPerDim, self.numofClusters, self.windowSize
+                    # print self.clustersUV.shape
+                    # exit(1)
+                    # print windowIdx
+                    self.segmentedImage[row,col,1] = self.clustersUV[windowIdx, 1]
+                    self.segmentedImage[row,col,2] = self.clustersUV[windowIdx, 0]
+                    self.segmentedImage[row,col,0] = self.image[row,col,0]
+
+
 
     def __findCntrMass__(self, window):
         """
@@ -86,6 +133,9 @@ class meanShiftSeg:
         :param window:
         :return:
         """
+        # print window.shape
+        # exit(130)
+        # assert window.shape[0] == 0, 'Window of empty'
         momntIdx = range( self.windowSize )
         nonZeroIdx = np.transpose(np.nonzero( window ))
         totalMass = np.max(np.cumsum( window ))
@@ -95,11 +145,10 @@ class meanShiftSeg:
             #Moment around column #0 ( around the x-axis )
             momentCol = np.max(np.cumsum(window.cumsum( axis=0 )[self.windowSize-1]*momntIdx))
             cntrCol = np.round(1.0*momentCol/totalMass)
-            # print cntrCol
             #Moment around row #0 ( around the y-axis )
             momentRow = np.max(np.cumsum(window.cumsum( axis=1 )[:,self.windowSize-1]*momntIdx))
             cntrRow = np.round(1.0*momentRow/totalMass)
-            # print cntrRow
+
             return cntrRow, cntrCol
     def __findEclidDist__(self, row, col):
         """
